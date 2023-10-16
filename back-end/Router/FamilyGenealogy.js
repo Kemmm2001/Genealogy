9fe1ccb30ed6715aac9651e9a8e093a9b1809ac5
TaNhatAnh <tanhatanh76@gmail.com>
2023-10-16 21:08:02 +0700

    article management

--- back-end/Router/FamilyGenealogy.js
+++ back-end/Router/FamilyGenealogy.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ManagerFamilyTree = require('../Controller/FamilyGenealogy/FamilyTree')
 const FamilyMemberManagement = require('../Controller/FamilyGenealogy/FamilyMember')
 const PdfController = require('../Controller/FamilyGenealogy/pdfController')
+const ArticleController = require('../Controller/FamilyGenealogy/ArticleController')
 var router = express.Router();
 
 
@@ -34,6 +35,11 @@ const initWebRouter = (app) => {
     router.post('/filter-member', FamilyMemberManagement.filterMember);
     router.post('/export-pdf', PdfController.exportPDF);
     
+    router.get('/article', ArticleController.getAllArticle);
+    router.post('/add-article', ArticleController.addArticle);
+    router.put('/update-article', ArticleController.updateArticle);
+    router.delete('/delete-article', ArticleController.deleteArticle);
+
     //Tiền tố đứng trước route
     app.use('/api/v1', router);
 }
