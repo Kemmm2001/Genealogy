338a9d33b969c3810ecfc844acd32f05049c1b00
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-10-17 22:05:14 +0700

    CRUD, read all, api for family history

---
+++ back-end/service/FamilyGenealogy/FamilyHistoryManagement.js
@@ -0,0 +1,106 @@
+const db = require('../../Models/ConnectDB')
+
+function getFamilyHistoryByCodeId(codeId) {
+    return new Promise((resolve, reject) => {
+        let query = `SELECT * FROM familyhistory where CodeID  = ${codeId}`
+        db.connection.query(query, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+function getFamilyHistoryById(historyId) {
+    return new Promise((resolve, reject) => {
+        let query = `SELECT * FROM familyhistory where HistoryID  = ${historyId}`
+        db.connection.query(query, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+function getAllFamilyHistory() {
+    return new Promise((resolve, reject) => {
+        let query = `SELECT * FROM familyhistory`
+        db.connection.query(query, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+function insertFamilyHistory(ObjData) {
+    return new Promise((resolve, reject) => {
+        let query = `INSERT INTO familyhistory (CodeID, Description) VALUES (?, ?);`
+        let values = [
+            ObjData.CodeID,
+            ObjData.Description
+        ]
+        db.connection.query(query, values, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    });
+
+}
+
+function updateFamilyHistory(ObjData) {
+    return new Promise((resolve, reject) => {
+        let query = `UPDATE familyhistory SET CodeID = ?, Description = ? WHERE HistoryID = ?;`
+        let values = [
+            ObjData.CodeID,
+            ObjData.Description,
+            ObjData.FamilyHistoryID
+        ]
+        db.connection.query(query, values, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    });
+
+}
+
+function removeFamilyHistory(historyId) {
+    return new Promise((resolve, reject) => {
+        let query = `DELETE FROM familyhistory WHERE HistoryID = ${historyId};`
+        db.connection.query(query, (err, result) => {
+            if (err) {
+                console.log("Have err : " + err);
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    });
+
+}
+
+module.exports = {
+    getFamilyHistoryByCodeId,
+    getFamilyHistoryById,
+    getAllFamilyHistory,
+    insertFamilyHistory,
+    updateFamilyHistory,
+    removeFamilyHistory
+}
