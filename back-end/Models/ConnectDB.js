2bfdb1fe0619f552faddbd1285eb5ee46ba09da3
Kemmm2001 <nguyenlehung2602@gmail.com>
2023-10-19 18:03:23 +0700

    Update code

--- back-end/Models/ConnectDB.js
+++ back-end/Models/ConnectDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Anhtuan13',
+  password: '123456',
   database: 'genealogy'
 });
 
