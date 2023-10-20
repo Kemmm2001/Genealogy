8a8cd833a8ac8e8ac1071f11e61bbdad7490eb55
Kemmm2001 <nguyenlehung2602@gmail.com>
2023-10-20 09:05:58 +0700

    Update code

--- back-end/Controller/FamilyGenealogy/FamilyMember.js
+++ back-end/Controller/FamilyGenealogy/FamilyMember.js
@@ -169,7 +169,7 @@ var getAllMember = async (req, res) => {
     try {
         // Gọi hàm từ dịch vụ để lấy tất cả thành viên
         const members = await FamilyManagementService.getAllMember();
-
+        console.log("abc")
         // Trả về danh sách thành viên trong phản hồi
         res.json({ success: true, data: members });
     } catch (error) {
