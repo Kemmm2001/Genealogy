fa5120fb40c45ff9db8d6bd8a555df9e303cca3d
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-11-08 00:53:57 +0700

    change url

--- front-end/src/assets/js/baseAPI.js
+++ front-end/src/assets/js/baseAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: `http://localhost:3003/api/v1`,
+  baseURL: `http://giaphanguoiviet.com/api/v1`,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
