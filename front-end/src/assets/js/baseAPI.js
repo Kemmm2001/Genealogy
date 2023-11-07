ae550459d004d955478dce2febf969cd597669c7
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-11-08 00:52:05 +0700

    add header

--- front-end/src/assets/js/baseAPI.js
+++ front-end/src/assets/js/baseAPI.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export const HTTP = axios.create({
   baseURL: `http://localhost:3003/api/v1`,
   headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   },
   responseType: "json",
 })
