8234a07705f2adbae413c7aa7583a8bdd48f6b58
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-11-14 02:05:50 +0700

    update email api

--- back-end/Utils/Response.js
+++ back-end/Utils/Response.js
@@ -1,6 +1,7 @@
 const db = require('../Models/ConnectDB');
 
 let coreResponse = (success, status_code, message, data) => {
+    console.log(`success: ${success}, status_code: ${status_code}, message: ${message}, data: ${data}`);
     return {
         success: success,
         status_code: status_code,
@@ -17,27 +18,23 @@ let coreErrorResponse = (status_code, message, data) => {
 let badRequestResponse = (data, message) => {
     if (message == null) message = "Yêu cầu không hợp lệ, vui lòng kiểm tra lại";
     if (data == null) return coreErrorResponse(400, message);
-    console.error(`${message} : ${data}`);
     return coreErrorResponse(400, message, data);
 }
 
 let dataNotFoundResponse = (data, message) => {
     if (message == null) message = "Không tìm thấy dữ liệu phù hợp";
     if (data == null) return coreErrorResponse( 404, message);
-    console.error(`${message} : ${data}`);
     return coreErrorResponse(404, message, data);
 }
 let successResponse = (data, message) => {
     if (message == null) message = "Thành công";
     if (data == null) return coreResponse(true, 200, message);
-    console.error(`${message} : ${data}`);
     return coreResponse(true, 200, message, data);
 }
 
 let internalServerErrorResponse = (data, message) => {
     if (message == null) message = "Lỗi hệ thống";
     if (data == null) return coreErrorResponse(500, message);
-    console.error(`${message} : ${data}`);
     return coreErrorResponse(500, message, data);
 }
 
