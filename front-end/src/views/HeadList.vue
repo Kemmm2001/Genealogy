2ddcce2aa1021381a70936fa3087bdfafa7aa746
luutunglam2001 <luutunglam2001@gmail.com>
2023-11-14 18:27:50 +0700

    them router memberList

--- front-end/src/views/HeadList.vue
+++ front-end/src/views/HeadList.vue
@@ -668,9 +668,7 @@ export default {
       }else{
         formattedDate = dateString
       }
-      
       const date = new Date(formattedDate);
-
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
