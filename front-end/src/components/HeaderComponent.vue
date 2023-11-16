364359fd9cbd0354b7d61224e03edcfefae5b982
unknown <baolan0598@gmail.com>
2023-11-17 01:03:38 +0700

    Sua selected header nhung chua hoafn thanh

--- front-end/src/components/HeaderComponent.vue
+++ front-end/src/components/HeaderComponent.vue
@@ -23,11 +23,14 @@
         <div @mouseenter="expandFamilyInfo = true" @mouseleave="expandFamilyInfo = false"
           class="navbar-item d-flex py-2 position-relative" style="margin-left: 8px"
           @click="setSelectedHead('/information/headlist')"
-          :class="{ 'header-selected': currentPath === '/information/headlist' }">
-          <svg class="navbar-icon-sm" style="margin-left: 8px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-            <path
-              d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z" />
-          </svg>
+          :class="{ 'header-selected': currentPath === '/information/headlist' || currentPath === '/information/albumlist' || currentPath === '/information/articlelist' }">
+          <router-link to="/information/headlist">
+            <svg class="navbar-icon-sm" style="margin-left: 8px;" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 512">
+              <path
+                d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z" />
+            </svg>
+          </router-link>
           <div class="navbar-item-content d-flex align-items-center">
             <router-link to="/information/headlist">
               <div class="mx-2 navbar-text">Thông tin dòng họ</div>
@@ -40,16 +43,20 @@
               </router-link>
               <div v-show="expandFamilyInfo" class="navbar-extended-content p-2 m-1">Lịch sử dòng họ</div>
               <router-link to="/information/albumlist">
-                <div v-show="expandFamilyInfo" class="navbar-extended-content p-2 m-1">Album dòng họ</div>
+                <div @click="setSelectedHead('/information/albumlist')"
+                  :class="{ 'header-selected': currentPath === '/information/albumlist' }" v-show="expandFamilyInfo"
+                  class="navbar-extended-content p-2 m-1">Album dòng họ</div>
               </router-link>
               <router-link to="/information/articlelist">
-                <div v-show="expandFamilyInfo" class="navbar-extended-content p-2 m-1">Tư liệu dòng họ</div>
+                <div @click="setSelectedHead('/information/articlelist')"
+                  :class="{ 'header-selected': currentPath === '/information/articlelist' }" v-show="expandFamilyInfo"
+                  class="navbar-extended-content p-2 m-1">Tư liệu dòng họ</div>
               </router-link>
             </div>
           </div>
           <!-- <div class="navbar-item-content d-flex align-items-center position-absolute p-2">Thông tin dòng họ</div> -->
         </div>
-        <div @mouseenter="expandMemberList = true" @mouseleave="expandMemberList = false" class="navbar-item d-flex py-2"
+        <div :class="{ 'header-selected': currentPath === '/memberlist' }" @click="setSelectedHead('/memberlist')" @mouseenter="expandMemberList = true" @mouseleave="expandMemberList = false" class="navbar-item d-flex py-2"
           style="margin-left: 8px">
           <svg class="navbar-icon-sm" style="margin-left: 8px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
             <path
@@ -61,7 +68,7 @@
             </div>
           </router-link>
         </div>
-        <div @mouseenter="expandEventList = true" @mouseleave="expandEventList = false" class="navbar-item d-flex py-2"
+        <div :class="{ 'header-selected': currentPath === '/event' }" @click="setSelectedHead('/event')" @mouseenter="expandEventList = true" @mouseleave="expandEventList = false" class="navbar-item d-flex py-2"
           style="margin-left: 8px">
           <svg class="navbar-icon-sm" style="margin-left: 8px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path
