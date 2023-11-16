414a235c8db6e99821ea0b993679006cf2ad898d
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-11-16 18:58:18 +0700

    family album update

--- back-end/Controller/InformationGenealogy/AlbumPhotoController.js
+++ back-end/Controller/InformationGenealogy/AlbumPhotoController.js
@@ -19,6 +19,10 @@ var addAlbumPhoto = async (req, res) => {
             return res.send(Response.missingFieldsErrorResponse(missingFields));
         }
         console.log("No missing fields");
+        let checkCodeIDExistResponse = await checkCodeIDExist(req);
+        if (checkCodeIDExistResponse.success === false) {
+            return res.send(checkCodeIDExistResponse);
+        }
         // Thêm thông tin vào bảng albumphoto
         let data = await AlbumPhotoManagementService.insertAlbumPhoto(req.body);
         dataRes = {
@@ -28,7 +32,7 @@ var addAlbumPhoto = async (req, res) => {
         return res.send(Response.successResponse(dataRes));
     } catch (e) {
         console.log("Error: " + e);
-        return res.send(Response.internalServerErrorResponse(e));
+        return res.send(Response.internalServerErrorResponse());
     }
 };
 
@@ -53,10 +57,9 @@ var updateAlbumPhoto = async (req, res) => {
         }
         console.log("No missing fields");
 
-        // lấy thông tin AlbumPhoto từ database
-        let data = await AlbumPhotoManagementService.getAlbumPhotoById(req.body.AlbumID)
-        if (data == null || data.length == 0) {
-            return res.send(Response.dataNotFoundResponse());
+        let checkAlbumExistResponse = await checkAlbumExist(req);
+        if (checkAlbumExistResponse.success === false) {
+            return res.send(checkAlbumExistResponse);
         }
         // cập nhật AlbumPhoto vào database
         let dataUpdate = await AlbumPhotoManagementService.updateAlbumPhoto(req.body)
@@ -66,9 +69,62 @@ var updateAlbumPhoto = async (req, res) => {
         return res.send(Response.successResponse(dataRes));
     } catch (e) {
         console.log("Error: " + e);
-        return res.send(Response.internalServerErrorResponse(e));
+        return res.send(Response.internalServerErrorResponse());
     }
 };
+
+var checkAlbumExist = async (req) => {
+    try {
+        let checkAlbumIDExistResponse = await checkAlbumIDExist(req);
+        if (checkAlbumIDExistResponse.success === false) {
+            return checkAlbumIDExistResponse;
+        }
+        let checkCodeIDExistResponse = await checkCodeIDExist(req);
+        if (checkCodeIDExistResponse.success === false) {
+            return checkCodeIDExistResponse;
+        }
+        return Response.successResponse();
+    } catch (e) {
+        console.log("Error: " + e);
+        return Response.internalServerErrorResponse();
+    }
+}
+
+var checkAlbumIDExist = async (req) => {
+    try {
+        let dataAlbumID;
+        if (req.body.AlbumID != null && req.body.AlbumID != undefined && req.body.AlbumID !== "") {
+            console.log(`Get AlbumPhoto by AlbumID : ${req.body.AlbumID}`);
+            dataAlbumID = await AlbumPhotoManagementService.getAlbumPhotoById(req.body.AlbumID)
+        }
+        console.log("dataAlbumID: " + dataAlbumID);
+        if (dataAlbumID == null || dataAlbumID.length == 0) {
+            return Response.dataNotFoundResponse();
+        }
+        return Response.successResponse();
+    } catch (e) {
+        console.log("Error: " + e);
+        return Response.internalServerErrorResponse();
+    }
+}
+
+var checkCodeIDExist = async (req) => {
+    try {
+        let dataCodeID;
+        if (req.body.CodeID != null && req.body.CodeID && req.body.CodeID !== "") {
+            console.log(`Get AlbumPhoto by CodeID : ${req.body.CodeID}`);
+            dataCodeID = await AlbumPhotoManagementService.getAlbumPhotoByCodeId(req.body.CodeID)
+        }
+        console.log("dataCodeID: " + dataCodeID);
+        if (dataCodeID == null || dataCodeID.length == 0) {
+            return Response.dataNotFoundResponse();
+        }
+        return Response.successResponse();
+    } catch (e) {
+        console.log("Error: " + e);
+        return Response.internalServerErrorResponse();
+    }
+}
 
 var deleteAlbumPhoto = async (req, res) => {
     try {
@@ -86,10 +142,9 @@ var deleteAlbumPhoto = async (req, res) => {
             return res.send(Response.missingFieldsErrorResponse(missingFields));
         }
         console.log("No missing fields");
-        // lấy thông tin AlbumPhoto từ database
-        let data = await AlbumPhotoManagementService.getAlbumPhotoById(req.query.AlbumID)
-        if (data == null || data.length == 0) {
-            return res.send(Response.dataNotFoundResponse());
+        let checkAlbumIDExistResponse = await checkAlbumIDExist(req);
+        if (checkAlbumIDExistResponse.success === false) {
+            return res.send(checkAlbumIDExistResponse);
         }
         // xóa AlbumPhoto khỏi database
         let dataDelete = await AlbumPhotoManagementService.removeAlbumPhoto(req.query.AlbumID)
@@ -99,25 +154,24 @@ var deleteAlbumPhoto = async (req, res) => {
         return res.send(Response.successResponse(dataRes));
     } catch (e) {
         console.log("Error: " + e);
-        return res.send(Response.internalServerErrorResponse(e));
+        return res.send(Response.internalServerErrorResponse());
     }
 };
 
 
 var getAlbumPhoto = async (req, res) => {
     try {
-        let reqData = req.query;
         // Log ra thông tin trong req.query
-        console.log('Request query: ', reqData);
+        console.log('Request query: ', req.query);
         // lấy thông tin AlbumPhoto từ database
         // những thông tin có thể lấy được : AlbumID, CodeID
         let data;
-        if (reqData.AlbumID != null && reqData.AlbumID != undefined && reqData.AlbumID !== "") {
-            console.log(`Get AlbumPhoto by AlbumID : ${reqData.AlbumID}`);
-            data = await AlbumPhotoManagementService.getAlbumPhotoById(reqData.AlbumID)
-        }else if (reqData.CodeID != null && reqData.CodeID && reqData.CodeID !== "") {
-            console.log(`Get AlbumPhoto by CodeID : ${reqData.CodeID}`);
-            data = await AlbumPhotoManagementService.getAlbumPhotoByCodeId(reqData.CodeID)
+        if (req.query.AlbumID != null && req.query.AlbumID != undefined && req.query.AlbumID !== "") {
+            console.log(`Get AlbumPhoto by AlbumID : ${req.query.AlbumID}`);
+            data = await AlbumPhotoManagementService.getAlbumPhotoById(req.query.AlbumID)
+        } else if (req.query.CodeID != null && req.query.CodeID && req.query.CodeID !== "") {
+            console.log(`Get AlbumPhoto by CodeID : ${req.query.CodeID}`);
+            data = await AlbumPhotoManagementService.getAlbumPhotoByCodeId(req.query.CodeID)
         }
         console.log("data: " + data);
         if (data == null || data.length == 0) {
@@ -127,7 +181,7 @@ var getAlbumPhoto = async (req, res) => {
         }
     } catch (e) {
         console.log("Error: " + e);
-        return res.send(Response.internalServerErrorResponse(e));
+        return res.send(Response.internalServerErrorResponse());
     }
 };
 
@@ -138,7 +192,7 @@ var getAllAlbumPhotos = async (req, res) => {
         return res.send(Response.successResponse(data));
     } catch (e) {
         console.log("Error: " + e);
-        return res.send(Response.internalServerErrorResponse(e));
+        return res.send(Response.internalServerErrorResponse());
     }
 };
 
--- back-end/service/InformationGenealogy/AlbumPhotoManagement.js
+++ back-end/service/InformationGenealogy/AlbumPhotoManagement.js
@@ -2,123 +2,165 @@ const db = require('../../Models/ConnectDB')
 
 function getAlbumPhotoByCodeId(codeId) {
     return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM albumphoto where CodeID  = ${codeId}`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
+        try {
+            let query = `SELECT * FROM albumphoto where CodeID  = ${codeId}`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject()
+                } else {
+                    resolve(result)
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     })
 }
 
 function getAlbumPhotoById(albumId) {
     return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM albumphoto where AlbumID  = ${albumId}`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
+        try {
+            let query = `SELECT * FROM albumphoto where AlbumID  = ${albumId}`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject()
+                } else {
+                    resolve(result)
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     })
 }
 
 function getAlbumPhotoByCodeId(codeId) {
     return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM albumphoto where CodeID  = ${codeId}`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
+        try {
+            let query = `SELECT * FROM albumphoto where CodeID  = ${codeId}`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject()
+                } else {
+                    resolve(result)
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     })
 }
 function getAlbumPhotoByIdAndCodeId(albumId, codeId) {
     return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM albumphoto where AlbumID  = ${albumId} and CodeID = ${codeId}`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
+        try {
+            let query = `SELECT * FROM albumphoto where AlbumID  = ${albumId} and CodeID = ${codeId}`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject()
+                } else {
+                    resolve(result)
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     })
 }
 
 function getAllAlbumPhoto() {
     return new Promise((resolve, reject) => {
-        let query = `SELECT * FROM albumphoto`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
+        try {
+            let query = `SELECT * FROM albumphoto`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject()
+                } else {
+                    resolve(result)
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     })
 }
 
 function insertAlbumPhoto(ObjData) {
     return new Promise((resolve, reject) => {
-        let query = `INSERT INTO albumphoto (AlbumName, CodeID) VALUES (?, ?);`
-        let values = [
-            ObjData.AlbumName,
-            ObjData.CodeID
-        ]
-        db.connection.query(query, values, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        })
+        try {
+            let query = `INSERT INTO albumphoto (AlbumName, CodeID, description) VALUES (?, ?,?);`
+            let values = [
+                ObjData.AlbumName,
+                ObjData.CodeID,
+                ObjData.Description
+            ]
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject();
+                } else {
+                    resolve(result);
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     });
 
 }
 
 function updateAlbumPhoto(ObjData) {
     return new Promise((resolve, reject) => {
-        let query = `UPDATE albumphoto SET AlbumName = ?, CodeID = ? WHERE AlbumID = ?;`
-        let values = [
-            ObjData.AlbumName,
-            ObjData.CodeID,
-            ObjData.AlbumID
-        ]
-        db.connection.query(query, values, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        })
+        try {
+            let query = `UPDATE albumphoto SET AlbumName = ?, CodeID = ?, Description = ? WHERE AlbumID = ?;`
+            let values = [
+                ObjData.AlbumName,
+                ObjData.CodeID,
+                ObjData.Description,
+                ObjData.AlbumID
+            ]
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject();
+                } else {
+                    resolve(result);
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     });
 
 }
 
 function removeAlbumPhoto(albumId) {
     return new Promise((resolve, reject) => {
-        let query = `DELETE FROM albumphoto WHERE AlbumID = ${albumId};`
-        db.connection.query(query, (err, result) => {
-            if (err) {
-                console.log("Have err : " + err);
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        })
+        try {
+            let query = `DELETE FROM albumphoto WHERE AlbumID = ${albumId};`
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log("Have err : " + err);
+                    reject();
+                } else {
+                    resolve(result);
+                }
+            })
+        } catch (e) {
+            console.log("Have err : " + e);
+            reject()
+        }
     });
 
 }
