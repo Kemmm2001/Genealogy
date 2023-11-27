e5465fb1efa711f71d98ca42c59b9126016e330a
luutunglam2001 <luutunglam2001@gmail.com>
2023-11-27 16:05:41 +0700

    them icon

--- front-end/src/views/MainScreen.vue
+++ front-end/src/views/MainScreen.vue
@@ -1117,6 +1117,11 @@ export default {
       FamilyTree.templates.tommy_female.field_2 =
         '<text class="field_4" style="font-size: 14px;" fill="#ffffff" x="90" y="80">Đời: {val}</text>';
 
+      var callIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>`;
+      FamilyTree.templates.tommy_male.isGG = '<g transform="translate(220,10)";>' + callIcon + '</g>';
+
       FamilyTree.elements._textbox = FamilyTree.elements.textbox;
       FamilyTree.elements.textbox = function (param1, param2, param3) {
         if (param2 && param2.label == FamilyTree.SEARCH_PLACEHOLDER) {
@@ -1135,6 +1140,7 @@ export default {
           img_0: "img",
           field_1: "dob",
           field_2: "generation",
+          isGG:"isGG"
         },
 
         // lazyLoading: false,
@@ -2164,6 +2170,7 @@ export default {
               }
             }
             this.nodes[0].tags.push("great-grandfather");
+            this.nodes[0].isGG = "true";
             this.mytree(this.$refs.tree, this.nodes);
           }
           // this.family.load(this.nodes);
