6ad6457d9bf9eca020ac6626478bc61ddd37dd93
nguyetuan <tuannahe150029@fpt.edu.vn>
2023-11-28 14:54:12 +0700

    update marriage mamagement

--- back-end/service/FamilyGenealogy/MarriageManagement.js
+++ back-end/service/FamilyGenealogy/MarriageManagement.js
@@ -13,7 +13,7 @@ const addMarriage = (data) => {
                 if (err) {
                     console.log(err);
                     reject(err);
-                }else{
+                } else {
                     resolve(result);
                 }
             });
@@ -26,98 +26,170 @@ const addMarriage = (data) => {
 
 // Update a marriage record
 const updateMarriage = async (data) => {
-    try {
-        const { husbandID, wifeID, codeID, marriageID } = data;
-        const query = "UPDATE marriage SET husbandID = ?, wifeID = ?, codeID = ? WHERE marriageID = ?";
-        const values = [husbandID, wifeID, codeID, marriageID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const { husbandID, wifeID, codeID, marriageID } = data;
+            const query = "UPDATE marriage SET husbandID = ?, wifeID = ?, codeID = ? WHERE marriageID = ?";
+            const values = [husbandID, wifeID, codeID, marriageID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Delete a marriage record
 const deleteMarriage = async (marriageID) => {
-    try {
-        const query = "DELETE FROM marriage WHERE marriageID = ?";
-        const values = [marriageID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "DELETE FROM marriage WHERE marriageID = ?";
+            const values = [marriageID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get all marriage records
 const getAllMarriages = async () => {
-    try {
-        const query = "SELECT * FROM marriage";
-        const result = db.connection.query(query);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage";
+            db.connection.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get a marriage record by husbandID
 const getMarriageByHusbandID = async (husbandID) => {
-    try {
-        const query = "SELECT * FROM marriage WHERE husbandID = ?";
-        const values = [husbandID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage WHERE husbandID = ?";
+            const values = [husbandID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get a marriage record by wifeID
 const getMarriageByWifeID = async (wifeID) => {
-    try {
-        const query = "SELECT * FROM marriage WHERE wifeID = ?";
-        const values = [wifeID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage WHERE wifeID = ?";
+            const values = [wifeID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get a marriage record by codeID
 const getMarriageByCodeID = async (codeID) => {
-    try {
-        const query = "SELECT * FROM marriage WHERE codeID = ?";
-        const values = [codeID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage WHERE codeID = ?";
+            const values = [codeID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get a marriage record by marriageID
 const getMarriageByMarriageID = async (marriageID) => {
-    try {
-        const query = "SELECT * FROM marriage WHERE marriageID = ?";
-        const values = [marriageID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage WHERE marriageID = ?";
+            const values = [marriageID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Get a marriage record by husbandID and wifeID
 const getMarriageByHusbandIDAndWifeID = async (husbandID, wifeID) => {
-    try {
-        const query = "SELECT * FROM marriage WHERE husbandID = ? AND wifeID = ?";
-        const values = [husbandID, wifeID];
-        const result = db.connection.query(query, values);
-        return result;
-    } catch (error) {
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            const query = "SELECT * FROM marriage WHERE husbandID = ? AND wifeID = ?";
+            const values = [husbandID, wifeID];
+            db.connection.query(query, values, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
 };
 
 // Export the CRUD operations
