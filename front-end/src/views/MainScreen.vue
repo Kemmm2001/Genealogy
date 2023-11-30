9001cce2dbf4f782701449bfda3dee2a476472a5
luutunglam2001 <luutunglam2001@gmail.com>
2023-11-30 20:20:04 +0700

    update tree

--- front-end/src/views/MainScreen.vue
+++ front-end/src/views/MainScreen.vue
@@ -1145,7 +1145,7 @@ export default {
           field_2: "generation",
           isGG: "isGG",
         },
-
+        sticky: false,
         // lazyLoading: false,
         nodeMouseClick: FamilyTree.action.none,
       });
