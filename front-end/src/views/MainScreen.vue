23e131fd8327aa6778abe43b19e323d50a05de83
unknown <baolan0598@gmail.com>
2023-12-02 17:25:52 +0700

    Sua list

--- front-end/src/views/MainScreen.vue
+++ front-end/src/views/MainScreen.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="d-flex h-100 w-100 position-relative">
     <div class="list h-100 d-flex flex-column align-items-center">
-      <div v-if="memberRole != 3" class="w-100 d-flex flex-row" style="padding-top: 8px">
+      <div v-if="memberRole != 3" class="w-100 d-flex flex-column" style="height: 20%">
+      <div class="w-100 d-flex flex-row" style="padding-top: 8px">
         <div class="col-6" style="padding-left: 8px; padding-right: 6px">
           <div class="w-100 h-100">
             <button @click="openNotiModal()" style="width:100%" type="button" class="btn btn-secondary h-100">Tạo thông báo</button>
@@ -13,7 +14,7 @@
           </div>
         </div>
       </div>
-      <div v-if="memberRole != 3" class="w-100 d-flex flex-row" style="padding-top: 8px">
+      <div class="w-100 d-flex flex-row" style="padding-top: 8px">
         <div class="col-6" style="padding-left: 8px; padding-right: 6px">
           <div class="w-100 h-100">
             <button @click="BackUpdata()" style="width:100%;color:white" type="button" class="btn btn-secondary h-100">Lưu trữ dữ liệu</button>
@@ -25,7 +26,8 @@
           </div>
         </div>
       </div>
-      <div class="w-100 d-flex flex-column px-2" style="padding: 12px; flex-grow: 1; font-family: 'QuicksandBold', sans-serif;">
+      </div>
+      <div class="w-100 d-flex flex-column px-2" :class="{height100 : memberRole == 3}" style="padding: 12px; height: 80%; font-family: 'QuicksandBold', sans-serif;">
         <div class="existing-members d-flex flex-column w-100">
           <div class="d-flex align-items-center justify-content-center px-2 py-1 list-title">Thành viên có trên phả đồ</div>
           <div class="d-flex flex-column w-100" style="overflow-y: auto;cursor: pointer">
