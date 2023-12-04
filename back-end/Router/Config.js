bb5d49a391535ed8b806a3c9370f153c3d735a0d
MSI\admin <tanhatanh76@gmail.com>
2023-12-04 22:10:46 +0700

    update

--- back-end/Router/Config.js
+++ back-end/Router/Config.js
@@ -2,6 +2,8 @@ const express = require('express');
 const PdfController = require('../Controller/FamilyGenealogy/pdfController')
 const ExcelController = require('../Controller/EventGenealogy/excelController')
 const JsonController = require('../Controller/Others/JsonController')
+const CoreFunction = require('../Utils/CoreFunction');
+
 var router = express.Router();
 
 
@@ -11,7 +13,7 @@ const initWebRouter = (app) => {
     router.post('/export-pdf', PdfController.exportPDF);
     router.post('/export-excle', ExcelController.exportExcel);
     router.post('/back-up', JsonController.exportData)
-    router.post('/import', JsonController.importData)
+    router.post('/import',CoreFunction.uploadExcelFile('file').single('xlsx'), JsonController.importData)
 
     //Tiền tố đứng trước route
     app.use('/api/v1', router);
