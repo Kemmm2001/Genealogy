e44284f9bffadf022bc36294bed08cf11821b0f6
Kemmm2001 <nguyenlehung2602@gmail.com>
2023-12-13 21:00:14 +0700

    Update time type send message

--- front-end/src/views/FamilyEvent.vue
+++ front-end/src/views/FamilyEvent.vue
@@ -319,9 +319,9 @@
                   </div>
                   <div class="col-3 d-flex flex-row px-2" style="padding-bottom: 4px;">
                     <select v-model="timeType" class="form-select h-100 w-100">
+                      <option value="m">phút</option>
                       <option value="h">Giờ</option>
-                      <option value="d">Ngày</option>
-                      <option value="m">Tháng</option>
+                      <option value="d">Ngày</option>                      
                     </select>
                   </div>
                 </div>
