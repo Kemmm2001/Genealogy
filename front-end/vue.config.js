1ef8989e7cb58ac1a04dfc097102e0dbee3b0a76
Nguyen Anh Tuan <85565421+nguyetuan@users.noreply.github.com>
2023-12-23 21:34:33 +0700

    Update vue.config.js

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
-    port: 3008,  
+    port: 80,  
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
 })
