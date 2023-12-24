97372a6916c9b4695766de9784011e00f0384399
unknown <baolan0598@gmail.com>
2023-12-24 16:25:44 +0700

    Sua fe

--- front-end/src/views/MainScreenResp.vue
+++ front-end/src/views/MainScreenResp.vue
@@ -94,7 +94,7 @@
           </div>
         </div>
         <div @click="advancedFilterDown = !advancedFilterDown" class="d-flex flex-column align-items-center justify-content-center h-100">
-          <div class="filter-text" v-if="!advancedFilterDown" style="color: white; font-family: 'QuicksandBold'">Bộ lọc nâng cao</div>
+          <div class="filter-text" v-if="!advancedFilterDown" style="color: white; font-family: 'QuicksandBold'">Bộ lọc</div>
           <div>
             <svg :class="{ rotateDown: !advancedFilterDown }" class="advanced-filter-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z" />
